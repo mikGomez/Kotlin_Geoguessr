7463f2ef14fa9065f2235b93bd3d7a819f4a324d
Miguel Gomez <miguel5121996@gmail.com>
2024-01-15 19:41:19 +0100

    Clase Slider para pasar imagenes, prueba de fotos (cambiarlas luego)

--- app/src/main/java/com/example/geoguessr/MainActivity.kt
+++ app/src/main/java/com/example/geoguessr/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.geoguessr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
 
 class MainActivity : AppCompatActivity() {
+    lateinit var viewPager: ViewPager
+    lateinit var viewPagerAdapter: ViewPagerAdapter
+    lateinit var imageList: List<Int>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewPager = findViewById(R.id.idViewPager)
+
+        imageList = ArrayList<Int>()
+        imageList = imageList + R.drawable.img1
+        imageList = imageList + R.drawable.img2
+        imageList = imageList + R.drawable.img3
+        imageList = imageList + R.drawable.img4
+        imageList = imageList + R.drawable.img5
+
+        viewPagerAdapter = ViewPagerAdapter(this@MainActivity, imageList)
+
+        viewPager.adapter = viewPagerAdapter
     }
 }
