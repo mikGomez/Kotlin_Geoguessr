39e966745d9bb121c86999daa5c7d0ab1a8b1112
Pablo <paablont@gmail.com>
2024-01-16 12:08:51 +0100

    Mapa cargado

--- app/src/main/java/com/example/geoguessr/MapsGame.kt
+++ app/src/main/java/com/example/geoguessr/MapsGame.kt
@@ -1,13 +1,54 @@
 package com.example.geoguessr
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
+import android.location.Location
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.geoguessr.databinding.ActivityLeverselectorBinding
+import com.example.geoguessr.databinding.ActivityMapsGameBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CircleOptions
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PointOfInterest
+import com.google.android.gms.maps.model.PolylineOptions
 
-class MapsGame : AppCompatActivity() {
+class MapsGame : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener, GoogleMap.OnPoiClickListener, GoogleMap.OnMapLongClickListener, GoogleMap.OnMarkerClickListener {
+
+    private val LOCATION_REQUEST_CODE: Int = 0
+    private lateinit var mapView: MapView
+    private lateinit var map: GoogleMap
+
+    var alMarcadores = ArrayList<Marker>()
+    lateinit var binding: ActivityMapsGameBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps_game)
+        binding = ActivityMapsGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        mapView = findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
         val position = intent.getIntExtra("IMAGE_POSITION", -1)
         when(position){
             0-> launch1()
@@ -32,5 +73,193 @@ class MapsGame : AppCompatActivity() {
 
     private fun launch1() {
 
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val location = LatLng(37.7749, -122.4194) // Coordenadas de San Francisco, por ejemplo
+
+        //googleMap.addMarker(MarkerOptions().position(location).title("Marker in San Francisco"))
+        //googleMap.moveCamera(CameraUpdateFactory.newLatLng(location))
+
+        map = googleMap
+        //Se pueden seleccionar varios tiops de mapas:
+        //  None --> no muestra nada, solo los marcadores. (MAP_TYPE_NONE)
+        //  Normal --> El mapa por defecto. (MAP_TYPE_NORMAL)
+        //  Satélite --> Mapa por satélite.  (MAP_TYPE_SATELLITE)
+        //  Híbrido --> Mapa híbrido entre Normal y Satélite. (MAP_TYPE_HYBRID) Muestra satélite y mapas de carretera, ríos, pueblos, etc... asociados.
+        //  Terreno --> Mapa de terrenos con datos topográficos. (MAP_TYPE_TERRAIN)
+        map.mapType = GoogleMap.MAP_TYPE_HYBRID
+        map.setOnMyLocationButtonClickListener(this)
+        map.setOnMyLocationClickListener(this)
+        map.setOnPoiClickListener(this)
+        map.setOnMapLongClickListener (this)
+        map.setOnMarkerClickListener(this)
+
+        enableMyLocation() //--> Hanilita, pidiendo permisos, la localización actual.
+        //irubicacioActual() //--> Nos coloca en la ubicación actual directamente. Comenta createMarker par ver esto.
+        //pintarCirculo()
+        //pintarRuta()
+    }
+
+
+
+    //----------------------------------------------------------------------------------------
+    @SuppressLint("MissingPermission")
+    fun enableMyLocation() {
+        if (!::map.isInitialized) return
+        if (isPermissionsGranted()) {
+            map.isMyLocationEnabled = true
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    /**
+     * función que usaremos a lo largo de nuestra app para comprobar si el permiso ha sido aceptado o no.
+     */
+    fun isPermissionsGranted() = ContextCompat.checkSelfPermission(
+        this,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    /**
+     * Método que solicita los permisos.
+     */
+    private fun requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            Toast.makeText(this, "Ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
+        } else {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_REQUEST_CODE)
+        }
+    }
+
+    /**
+     * Con este método vamos a ajustar el tamaño de todos los iconos que usemos en los marcadores.
+     */
+    fun sizeIcon(idImage:Int): BitmapDescriptor {
+        val altura = 60
+        val anchura = 60
+
+        var draw = ContextCompat.getDrawable(this,idImage) as BitmapDrawable
+        val bitmap = draw.bitmap  //Aquí tenemos la imagen.
+
+        //Le cambiamos el tamaño:
+        val smallBitmap = Bitmap.createScaledBitmap(bitmap, anchura, altura, false)
+        return BitmapDescriptorFactory.fromBitmap(smallBitmap)
+
+    }
+//-----------------------------------------------------------------------------------------------------
+    //----------------------------------------- Eventos en el mapa ----------------------------------------
+    //-----------------------------------------------------------------------------------------------------
+
+    /**
+     * Se dispara cuando pulsamos la diana que nos centra en el mapa (punto negro, arriba a la derecha en forma de diana).
+     */
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(this, "Recentrando", Toast.LENGTH_SHORT).show()
+        return false
+    }
+
+    /**
+     * Se dispara cuando pulsamos en nuestra localización exacta donde estámos ahora (punto azul).
+     */
+    override fun onMyLocationClick(p0: Location) {
+        Toast.makeText(this, "Estás en ${p0.latitude}, ${p0.longitude}", Toast.LENGTH_SHORT).show()
+    }
+
+    /**
+     * Con el parámetro podremos obtener información del punto de interés. Este evento se lanza cuando pulsamos en un POI.
+     */
+    override fun onPoiClick(p0: PointOfInterest) {
+        Toast.makeText(this, "Pulsado.", Toast.LENGTH_LONG).show()
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.run {
+            setTitle("Información del lugar.")
+            setMessage("Id: " + p0!!.placeId + "\nNombre: " + p0!!.name + "\nLatitud: " + p0!!.latLng.latitude.toString() + " \nLongitud: " + p0.latLng.longitude.toString())
+            setPositiveButton("Aceptar"){ dialog: DialogInterface, i:Int ->
+
+            }
+        }
+        dialogBuilder.create().show()
+
+    }
+
+    /**
+     * Con el parámetro crearemos un marcador nuevo. Este evento se lanzará al hacer un long click en alguna parte del mapa.
+     */
+    override fun onMapLongClick(p0: LatLng) {
+        var marcador = map.addMarker(MarkerOptions().position(p0!!).title("Nuevo marcador"))
+        alMarcadores.add(marcador!!)
+
+        Log.e("ACSCO","Marcador añadido, marcadores actuales: ${alMarcadores.toString()}")
+    }
+
+    /**
+     * Este evento se lanza cuando hacemos click en un marcador.
+     */
+    override fun onMarkerClick(p0: Marker): Boolean {
+        Toast.makeText(this, "Estás en ${p0!!.title}, ${p0!!.position}", Toast.LENGTH_SHORT).show()
+
+        p0.remove()  //---> Para borrarlo cuando hago click sobre él solo hay que descomentar esto.
+        alMarcadores.removeAt(alMarcadores.indexOf(p0))
+        Log.e("ACSCO","Marcador eliminado, marcadores actuales: ${alMarcadores.toString()}")
+
+        return true;
+    }
+
+    /**
+     * Nos coloca en la ubicación actual.
+     */
+    @SuppressLint("MissingPermission")
+    private fun irubicacioActual() {
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val miUbicacion = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        val latLng = LatLng(miUbicacion!!.latitude, miUbicacion.longitude)
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18f)) //--> Mueve la cámara a esa posición, sin efecto. El valor real indica el nivel de Zoom, de menos a más.
+    }
+
+    //------------------------------------------------------------------------------------------------------
+
+    /**
+     * Dibuja una línea recta desde nuestra ubicación actual al CIFP Virgen de Gracia.
+     */
+    @SuppressLint("MissingPermission")
+    fun pintarRuta(){
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val miUbicacion = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        val latLng = LatLng(miUbicacion!!.latitude, miUbicacion.longitude)
+        val markerInstituto = LatLng(38.991030,-3.920489)
+
+        map.addPolyline(PolylineOptions().run{
+            add(latLng, markerInstituto)
+            color(Color.BLUE)
+            width(9f)
+        })
+
+        val loc1 = Location("")
+        loc1.latitude = latLng.latitude
+        loc1.longitude = latLng.longitude
+        val loc2 = Location("")
+        loc2.latitude = markerInstituto.latitude
+        loc2.longitude = markerInstituto.longitude
+        val distanceInMeters = loc1.distanceTo(loc2)
+        Log.e("ACSCO", distanceInMeters.toString())
+    }
+
+    /**
+     * Dibuja una línea recta desde nuestra ubicación actual al CIFP Virgen de Gracia.
+     */
+    fun pintarCirculo(){
+        val markerInstituto = LatLng(38.991030,-3.920489)
+
+        map.addCircle(CircleOptions().run{
+            center(markerInstituto)
+            radius(9.0)
+            strokeColor(Color.BLUE)
+            fillColor(Color.GREEN)
+        })
     }
 }
