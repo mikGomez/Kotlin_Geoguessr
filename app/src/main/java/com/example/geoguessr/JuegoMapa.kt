25c68969dedbcd71f3e5ae21e70d881a78133444
Mikel <miguel5121996@gmail.com>
2024-01-16 10:51:57 +0100

    Prueba de Slider con opciones

---
+++ app/src/main/java/com/example/geoguessr/JuegoMapa.kt
@@ -0,0 +1,11 @@
+package com.example.geoguessr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JuegoMapa : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_juego_mapa)
+    }
+}
--- app/src/main/java/com/example/geoguessr/MainActivity.kt
+++ app/src/main/java/com/example/geoguessr/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.geoguessr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager.widget.ViewPager
 
+import com.example.geoguessr.databinding.ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     lateinit var viewPager: ViewPager
     lateinit var viewPagerAdapter: ViewPagerAdapter
     lateinit var imageList: List<Int>
@@ -25,5 +29,27 @@ class MainActivity : AppCompatActivity() {
         viewPagerAdapter = ViewPagerAdapter(this@MainActivity, imageList)
 
         viewPager.adapter = viewPagerAdapter
+
+        // Establece un listener para los clics en el ViewPager
+        viewPager.setOnClickListener {
+            // Obtiene la posición de la imagen actual en el ViewPager
+            val currentPosition = viewPager.currentItem
+
+            // Realiza alguna acción según la posición, por ejemplo, abre otra actividad
+            when (currentPosition) {
+                0 -> openActivity1()
+                1 -> openActivity2()
+            }
+        }
+    }
+
+    private fun openActivity1() {
+        val intent = Intent(this, JuegoMapa::class.java)
+        startActivity(intent)
+    }
+
+    private fun openActivity2() {
+        val intent = Intent(this, Login::class.java)
+        startActivity(intent)
     }
 }
